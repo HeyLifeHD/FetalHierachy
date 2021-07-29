96908d6f5a84f277b882e0ac58f10b485061d796
HeyLifeHD <joschkahey@gmail.com>
2021-07-29 09:47:42 +0200

    methylation calling

--- WGBS/scripts/processing/00_mBias_methCalling.sh
+++ WGBS/scripts/processing/00_mBias_methCalling.sh
@@ -11,49 +11,23 @@ for file in ls /icgc/dkfzlsdf/project/OE0219/mouse_hematopoiesis_PBAT/sequencing
 done 
 
 #Extract methylation values with adjusting for mbias
-mkdir icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/
-for file in ls /icgc/dkfzlsdf/project/OE0219/cancer_microenvironment_pbat/sequencing/whole_genome_bisulfite_tagmentation_sequencing/view-by-pid/*/*/paired/merged-alignment/.merging_0/*_merged.mdup.bam; do
+mkdir /omics/groups/OE0219/internal/FetalHierachy/WGBS/QC/methylDackel/methylationCalls/
+for file in ls /icgc/dkfzlsdf/project/OE0219/mouse_hematopoiesis_PBAT/sequencing/whole_genome_bisulfite_tagmentation_sequencing/view-by-pid/*fetal*/*/paired/merged-alignment/.merging_0/*_merged.mdup.bam; do
     fname=`basename $file`
     dirname=`echo $fname | awk -F\. '{print $1}'`
-    #mkdir /icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/${dirname}
+    mkdir  /omics/groups/OE0219/internal/FetalHierachy/WGBS/QC/methylDackel/methylationCalls/${dirname}
     MethylDackel extract -@ 5 --OT 6,0,6,140 --OB 2,145,12,150  \
     /icgc/dkfzlsdf/analysis/C010/genomes/Mmusculus/GRCm38/seq/GRCm38mm10_PhiX_Lambda.fa \
     $file \
-    -o /icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/${dirname}/${fname}
+    -o  /omics/groups/OE0219/internal/FetalHierachy/WGBS/QC/methylDackel/methylationCalls//${dirname}/${fname}
     echo $dirname
 
 
     echo $fname
 done 
-
 
 #add chr to seqnames
-for file in /icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/*/*.bedGraph; do
-awk '{print "chr" $0}' $file > $file.chr.bedgraph
-echo $file 
-done 
-
-
-#still missing:
-#/icgc/dkfzlsdf/project/OE0219/cancer_microenvironment_pbat/sequencing/whole_genome_bisulfite_tagmentation_sequencing/view-by-pid/C010_CME_PBAT_BMDM/tumor-rep2/paired/merged-alignment/tumor-rep2_C010_CME_PBAT_BMDM_merged.mdup.bam
-
-
-
-#Extract methylation values with adjusting for mbias
-mkdir icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/
-for file in /icgc/dkfzlsdf/project/OE0219/cancer_microenvironment_pbat/sequencing/whole_genome_bisulfite_tagmentation_sequencing/view-by-pid/C010_CME_PBAT_BMDM/tumor-rep2/paired/merged-alignment/.merging_0/tumor-rep2_C010_CME_PBAT_BMDM_merged.mdup.bam; do
-    fname=`basename $file`
-    dirname=`echo $fname | awk -F\. '{print $1}'`
-    #mkdir /icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/${dirname}
-    MethylDackel extract -@ 5 --OT 6,0,6,140 --OB 2,145,12,150  \
-    /icgc/dkfzlsdf/analysis/C010/genomes/Mmusculus/GRCm38/seq/GRCm38mm10_PhiX_Lambda.fa \
-    $file \
-    -o /icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/${dirname}/${fname}
-    echo $dirname
-    echo $fname
-done 
-
-for file in /icgc/dkfzlsdf/analysis/C010/cancer_microenvironment/TAM/WGBS/processing/PBAT/odcf/QC/methylDackel/methylationCalls/tumor-rep2_C010_CME_PBAT_BMDM_merged/*.bedGraph; do
+for file in /omics/groups/OE0219/internal/FetalHierachy/WGBS/QC/methylDackel//methylationCalls/*/*.bedGraph; do
 awk '{print "chr" $0}' $file > $file.chr.bedgraph
 echo $file 
 done 
