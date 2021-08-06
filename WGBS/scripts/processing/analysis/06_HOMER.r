b065b8414454c1d46190d5550a6c845d9176d734
HeyLifeHD <joschkahey@gmail.com>
2021-08-06 09:20:20 +0200

    homerTF

--- WGBS/scripts/processing/analysis/06_HOMER.r
+++ WGBS/scripts/processing/analysis/06_HOMER.r
@@ -43,23 +43,23 @@ for(i in names(dmrs_final)){
     dmrs_final_df[[i]] <- as.data.frame(dmrs_final[[i]])
     dmrs_final_df[[i]]$peak_id <- 1:nrow(dmrs_final_df[[i]])
 
-    dir.create(file.path(analysis.dir, "homer", "all"), recursive=TRUE)
-    dir.create(file.path(analysis.dir, "homer", "hypo"), recursive=TRUE)
-    dir.create(file.path(analysis.dir, "homer", "hyper"), recursive=TRUE)
+    dir.create(file.path(analysis.dir, "homer",i, "all"), recursive=TRUE)
+    dir.create(file.path(analysis.dir, "homer",i, "hypo"), recursive=TRUE)
+    dir.create(file.path(analysis.dir, "homer",i, "hyper"), recursive=TRUE)
     #all
     write.table(data.frame(chr=dmrs_final_df[[i]]$seqnames, start=dmrs_final_df[[i]]$start, end=dmrs_final_df[[i]]$end, id=dmrs_final_df[[i]]$peak_id, 
         notUsed=NA, strand="+"),
-        file.path(analysis.dir,  "homer", "all", paste0("DMRs.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
+        file.path(analysis.dir,  "homer", i, "all", paste0("DMRs.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
     #hypo
     write.table(data.frame(chr=dmrs_final_df[[i]][dmrs_final_df[[i]]$direction=="hypo",]$seqnames, 
         start=dmrs_final_df[[i]][dmrs_final[[i]]$direction=="hypo",]$start, end=dmrs_final_df[[i]][dmrs_final[[i]]$direction=="hypo",]$end, 
         id=dmrs_final_df[[i]][dmrs_final_df[[i]]$direction=="hypo",]$peak_id, notUsed=NA, strand="+"),
-        file.path(analysis.dir, "homer", "hypo", paste0("DMRs.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
+        file.path(analysis.dir, "homer", i,"hypo", paste0("DMRs.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
     #hyper
     write.table(data.frame(chr=dmrs_final_df[[i]][dmrs_final_df[[i]]$direction=="hyper",]$seqnames, 
         start=dmrs_final_df[[i]][dmrs_final_df[[i]]$direction=="hyper",]$start, end=dmrs_final_df[[i]][dmrs_final_df[[i]]$direction=="hyper",]$end, 
         id=dmrs_final_df[[i]][dmrs_final_df[[i]]$direction=="hyper",]$peak_id, notUsed=NA, strand="+"),
-        file.path(analysis.dir, "homer", "hyper", paste0("DMRs.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
+        file.path(analysis.dir, "homer", i,"hyper", paste0("DMRs.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
 }
 dmrs_red_df <- as.data.frame(dmrs_red)
 dmrs_red_df$peak_id <- 1:nrow(dmrs_red_df)
@@ -69,28 +69,29 @@ write.table(data.frame(chr=dmrs_red_df$seqnames,
         file.path(analysis.dir, "homer", paste0("DMRs_bg.bed")),sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
 
 #run in command line
-cd /omics/groups/OE0219/internal/FetalHierachy/WGBS/210729_analysis/DMRAnal
+cd /omics/groups/OE0219/internal/FetalHierachy/WGBS/210729_analysis/DMRAnal/homer
 chmod 777 -R ./
 conda activate homer2
 
-for file in `ls homer/*/DMRs.bed`
+for file in `ls */*/DMRs.bed`
 do
     echo ${file}
     path=`dirname ${file}`
-    findMotifsGenome.pl ${file} hg19 ${path} -size given -preparsedDir ${path}/ -p 6
+    findMotifsGenome.pl ${file} mm10 ${path} -size given -preparsedDir ${path}/ -p 10
     echo ${path}
+    echo $file
 done
 
 
 #with custom bg
-cd /omics/groups/OE0219/internal/FetalHierachy/WGBS/210729_analysis/DMRAnal
-chmod 777 -R ./homer
-conda activate homer2
+#cd /omics/groups/OE0219/internal/FetalHierachy/WGBS/210729_analysis/DMRAnal/homer
+chmod 777 -R ./
+#conda activate homer2
 
-for file in `ls homer/*/DMRs.bed`
+for file in `ls */*/DMRs.bed`
 do
     echo ${file}
     path=`dirname ${file}`
-    findMotifsGenome.pl ${file} hg19 ${path}_customBG -size given -preparsedDir ${path}/ -p 6 -bg /omics/groups/OE0219/internal/FetalHierachy/WGBS/210729_analysis/DMRAnal/homer/DMRs_bg.bed
+    findMotifsGenome.pl ${file} mm10 ${path}_customBG -size given -preparsedDir ${path}/ -p 10 -bg /omics/groups/OE0219/internal/FetalHierachy/WGBS/210729_analysis/DMRAnal/homer/DMRs_bg.bed
     echo ${path}
 done
